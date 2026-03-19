2c0b7170ae7ad9cfd29109eedbf3008f765a1379
Jandier Rojas Zúñiga <jandier.rojas.z@gmail.com>
2026-03-19 11:46:40 -0600

    The Publisher entity was created, along with its PublisherRepository and applied within the BootstrapData logic.

--- src/main/java/guru/springframework/spring_6_webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring_6_webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring_6_webapp.bootstrap;
 
 import guru.springframework.spring_6_webapp.domain.Author;
 import guru.springframework.spring_6_webapp.domain.Book;
+import guru.springframework.spring_6_webapp.domain.Publisher;
 import guru.springframework.spring_6_webapp.repositories.AuthorRepository;
 import guru.springframework.spring_6_webapp.repositories.BookRepository;
+import guru.springframework.spring_6_webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,13 @@ public class BootstrapData implements CommandLineRunner {
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
-        this.authorRepository = authorRepository;
+    public BootstrapData(PublisherRepository publisherRepository, BookRepository bookRepository,
+                         AuthorRepository authorRepository) {
+        this.publisherRepository = publisherRepository;
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
     }
 
     @Override
@@ -48,9 +53,24 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
 
+        Publisher morpho = new Publisher();
+        morpho.setPublisherName("Morpho");
+        morpho.setAddress("123 Main Street Anytown, NY 12345 United States");
+        morpho.setState("New York");
+        morpho.setCity("Manhattan");
+        morpho.setZip("90210");
+
+
+//        Publisher morphoSaved =
+        publisherRepository.save(morpho);
+
+//        morphoSaved.getBooks().add(noEJBSaved);
+//        morphoSaved.getBooks().add(dddSaved);
+
         System.out.println("In Bootstrap");
         System.out.println("Author count: " + authorRepository.count());
         System.out.println("Book count: " + bookRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
 
     }
 }
